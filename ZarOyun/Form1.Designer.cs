fd0918643a051b59572880ecd8c202348f35f172
Emir <emirkan.ulker9@gmail.com>
2023-10-26 08:19:12 +0300

    v.09

---
+++ ZarOyun/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace ZarOyun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnBaslaBen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label35 = new System.Windows.Forms.Label();
+            this.lblZ1zar = new System.Windows.Forms.Label();
+            this.lbl2Zar = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btnBaslaPc = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.lblpcTop = new System.Windows.Forms.Label();
+            this.lblPuan1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.lbl2Zar);
+            this.groupBox1.Controls.Add(this.lblZ1zar);
+            this.groupBox1.Controls.Add(this.label35);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnBaslaBen);
+            this.groupBox1.Location = new System.Drawing.Point(16, 18);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(374, 253);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ben";
+            // 
+            // btnBaslaBen
+            // 
+            this.btnBaslaBen.BackColor = System.Drawing.Color.Yellow;
+            this.btnBaslaBen.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBaslaBen.Location = new System.Drawing.Point(110, 177);
+            this.btnBaslaBen.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBaslaBen.Name = "btnBaslaBen";
+            this.btnBaslaBen.Size = new System.Drawing.Size(135, 57);
+            this.btnBaslaBen.TabIndex = 0;
+            this.btnBaslaBen.Text = "Start";
+            this.btnBaslaBen.UseVisualStyleBackColor = false;
+            this.btnBaslaBen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 93);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1. Zar:";
+            // 
+            // label35
+            // 
+            this.label35.AutoSize = true;
+            this.label35.Location = new System.Drawing.Point(231, 93);
+            this.label35.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label35.Name = "label35";
+            this.label35.Size = new System.Drawing.Size(46, 20);
+            this.label35.TabIndex = 2;
+            this.label35.Text = "1. Zar:";
+            // 
+            // lblZ1zar
+            // 
+            this.lblZ1zar.AutoSize = true;
+            this.lblZ1zar.Location = new System.Drawing.Point(117, 93);
+            this.lblZ1zar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblZ1zar.Name = "lblZ1zar";
+            this.lblZ1zar.Size = new System.Drawing.Size(16, 20);
+            this.lblZ1zar.TabIndex = 3;
+            this.lblZ1zar.Text = "0";
+            // 
+            // lbl2Zar
+            // 
+            this.lbl2Zar.AutoSize = true;
+            this.lbl2Zar.Location = new System.Drawing.Point(274, 93);
+            this.lbl2Zar.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl2Zar.Name = "lbl2Zar";
+            this.lbl2Zar.Size = new System.Drawing.Size(16, 20);
+            this.lbl2Zar.TabIndex = 4;
+            this.lbl2Zar.Text = "0";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.btnBaslaPc);
+            this.groupBox2.Location = new System.Drawing.Point(16, 332);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(374, 253);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Pc";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(262, 96);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(16, 20);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(105, 96);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(16, 20);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(219, 96);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(46, 20);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "1. Zar:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(63, 96);
+            this.label12.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(46, 20);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "1. Zar:";
+            // 
+            // btnBaslaPc
+            // 
+            this.btnBaslaPc.BackColor = System.Drawing.Color.Cyan;
+            this.btnBaslaPc.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBaslaPc.Location = new System.Drawing.Point(110, 177);
+            this.btnBaslaPc.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBaslaPc.Name = "btnBaslaPc";
+            this.btnBaslaPc.Size = new System.Drawing.Size(135, 57);
+            this.btnBaslaPc.TabIndex = 0;
+            this.btnBaslaPc.Text = "Start";
+            this.btnBaslaPc.UseVisualStyleBackColor = false;
+            this.btnBaslaPc.Click += new System.EventHandler(this.btnBaslaPc_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Lime;
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.lblPuan1);
+            this.groupBox3.Controls.Add(this.lblpcTop);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label14);
+            this.groupBox3.Location = new System.Drawing.Point(423, 332);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox3.Size = new System.Drawing.Size(478, 253);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Puan Tablosu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 138);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "2. Oyuncu:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(54, 77);
+            this.label14.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(71, 20);
+            this.label14.TabIndex = 1;
+            this.label14.Text = "1.Oyuncu:";
+            // 
+            // lblpcTop
+            // 
+            this.lblpcTop.AutoSize = true;
+            this.lblpcTop.Location = new System.Drawing.Point(133, 138);
+            this.lblpcTop.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblpcTop.Name = "lblpcTop";
+            this.lblpcTop.Size = new System.Drawing.Size(16, 20);
+            this.lblpcTop.TabIndex = 7;
+            this.lblpcTop.Text = "0";
+            // 
+            // lblPuan1
+            // 
+            this.lblPuan1.AutoSize = true;
+            this.lblPuan1.Location = new System.Drawing.Point(133, 77);
+            this.lblPuan1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPuan1.Name = "lblPuan1";
+            this.lblPuan1.Size = new System.Drawing.Size(16, 20);
+            this.lblPuan1.TabIndex = 8;
+            this.lblPuan1.Text = "0";
+            this.lblPuan1.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(450, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(172, 163);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(698, 68);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(172, 163);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(121, 195);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(914, 636);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "ZarSalla";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnBaslaBen;
+        private System.Windows.Forms.Label lbl2Zar;
+        private System.Windows.Forms.Label lblZ1zar;
+        private System.Windows.Forms.Label label35;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button btnBaslaPc;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblPuan1;
+        private System.Windows.Forms.Label lblpcTop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ZarOyun/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZarOyun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rastgele = new Random();
+        int toplamBen = 0;
+        int toplamPc = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnBaslaBen.Enabled = false;
+            btnBaslaPc.Enabled = true;
+            int a = rastgele.Next(1, 7);
+            int b = rastgele.Next(1, 7);
+            toplamBen = toplamBen + a + b;
+            lblPuan1.Text = toplamBen.ToString();
+            lblZ1zar.Text = a.ToString(); 
+            lbl2Zar.Text = b.ToString();
+
+            if (a == 1)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com.png";
+            }
+            if (a == 2)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (3).png";
+            }
+            if (a == 3)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (2).png";
+            }
+            if (a == 4)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (1).png";
+            }
+            if (a == 5)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (5).png";
+            }
+            if (a == 6)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (4).png";
+            }
+
+            if (b == 1)
+            {
+                pictureBox2.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com.png";
+            }
+            if (b == 2)
+            {
+                pictureBox2.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (3).png";
+            }
+            if (b == 3)
+            {
+                pictureBox2.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (2).png";
+            }
+            if (b == 4)
+            {
+                pictureBox2.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (1).png";
+            }
+            if (b == 5)
+            {
+                pictureBox2.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (5).png";
+            }
+            if (b == 6)
+            {
+                pictureBox2.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (4).png";
+            }
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBaslaPc_Click(object sender, EventArgs e)
+        {
+            btnBaslaPc.Enabled = false;
+            btnBaslaBen.Enabled = true;
+            int c = rastgele.Next(1, 7);
+            int d = rastgele.Next(1, 7);
+            label10.Text = c.ToString();
+            label9.Text = d.ToString();
+            toplamPc = toplamPc + c + d;
+            lblpcTop.Text = toplamPc.ToString();
+
+            if (c == 1)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com.png";
+            }
+            if (c == 2)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (3).png";
+            }
+            if (c == 3)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (2).png";
+            }
+            if (c == 4)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (1).png";
+            }
+            if (c == 5)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (5).png";
+            }
+            if (c == 6)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (4).png";
+            }
+            if (d == 1)
+            {
+                pictureBox2.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com.png";
+            }
+            if (d == 2)
+            {
+                pictureBox2.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (3).png";
+            }
+            if (d == 3)
+            {
+                pictureBox2.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (2).png";
+            }
+            if (d == 4)
+            {
+                pictureBox2.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (1).png";
+            }
+            if (d == 5)
+            {
+                pictureBox2.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (5).png";
+            }
+            if (d == 6)
+            {
+                pictureBox2.ImageLocation = "C:\\Users\\legen\\Downloads\\pngwing.com (4).png";
+            }
+            
+            if(toplamBen>=100 && toplamBen > toplamPc)
+            {
+                label3.Text = "Kazanan Sizsiniz Tebrikler Birinci Oyuncu!:)))))";
+                btnBaslaBen.Enabled = false;
+                btnBaslaPc.Enabled = false;
+            }
+            if(toplamPc>=100 && toplamPc > toplamBen)
+            {
+                label3.Text = "Kazanan Bilgisayar Siz Kaybettiniz :(((((";
+                btnBaslaBen.Enabled = false;
+                btnBaslaPc.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnBaslaBen.Enabled = true;
+            btnBaslaPc.Enabled = false;
+        }
+    }
+}
